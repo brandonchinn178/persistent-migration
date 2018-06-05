6c71dd2ffdd3859d74f23975bea6ae21d2f31565
Brandon Chinn <brandon@leapyear.io>
2018-06-05 15:21:49 -0700

    Add CREATE TABLE

--- src/Database/Persist/Migration/Postgres.hs
+++ src/Database/Persist/Migration/Postgres.hs
@@ -6,6 +6,10 @@ Portability :  portable
 
 Defines the migration backend for PostgreSQL.
 -}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Database.Persist.Migration.Postgres
   ( backend
@@ -13,17 +17,23 @@ module Database.Persist.Migration.Postgres
   , runMigration
   ) where
 
+import Data.Monoid ((<>))
 import Data.Text (Text)
+import qualified Data.Text as Text
 import Database.Persist.Migration
     ( AddColumn(..)
+    , Column(..)
+    , ColumnProp(..)
     , CreateTable(..)
     , DropColumn(..)
     , DropTable(..)
     , MigrateBackend(..)
     , Migration
+    , TableConstraint(..)
     )
 import qualified Database.Persist.Migration as Migration
-import Database.Persist.Sql (SqlPersistT)
+import Database.Persist.Migration.Sql (quote, uncommas)
+import Database.Persist.Sql (SqlPersistT, SqlType(..))
 
 -- | Run a migration with the Postgres backend.
 runMigration :: Migration -> SqlPersistT IO ()
@@ -43,7 +53,11 @@ backend = MigrateBackend
   }
 
 createTable' :: Bool -> CreateTable -> SqlPersistT IO [Text]
-createTable' = undefined
+createTable' ifNotExists CreateTable{..} = return
+  ["CREATE TABLE " <> ifNotExists' <> quote ctName <> "(" <> uncommas tableDefs <> ")"]
+  where
+    ifNotExists' = if ifNotExists then "IF NOT EXISTS " else ""
+    tableDefs = map showColumn ctSchema ++ map showTableConstraint ctConstraints
 
 dropTable' :: DropTable -> SqlPersistT IO [Text]
 dropTable' = undefined
@@ -53,3 +67,44 @@ addColumn' = undefined
 
 dropColumn' :: DropColumn -> SqlPersistT IO [Text]
 dropColumn' = undefined
+
+{- Helpers -}
+
+-- | Show a 'Column'.
+showColumn :: Column -> Text
+showColumn Column{..} =
+  Text.unwords
+    $ quote colName
+    : showSqlType colType
+    : map showColumnProp colProps
+
+-- | Show a 'SqlType'. See `showSqlType` from `Database.Persist.Postgresql`.
+showSqlType :: SqlType -> Text
+showSqlType = \case
+  SqlString -> "VARCHAR"
+  SqlInt32 -> "INT4"
+  SqlInt64 -> "INT8"
+  SqlReal -> "DOUBLE PRECISION"
+  SqlNumeric s prec -> "NUMERIC(" <> showT s <> "," <> showT prec <> ")"
+  SqlDay -> "DATE"
+  SqlTime -> "TIME"
+  SqlDayTime -> "TIMESTAMP WITH TIME ZONE"
+  SqlBlob -> "BYTEA"
+  SqlBool -> "BOOLEAN"
+  SqlOther (Text.toLower -> "integer") -> "INT4"
+  SqlOther t -> t
+  where
+    showT = Text.pack . show
+
+-- | Show a 'ColumnProp'.
+showColumnProp :: ColumnProp -> Text
+showColumnProp = \case
+  NotNull -> "NOT NULL"
+  Defaults def -> "DEFAULT " <> def
+  ForeignKey (tab, col) -> "REFERENCES " <> quote tab <> "(" <> quote col <> ")"
+
+-- | Show a `TableConstraint`.
+showTableConstraint :: TableConstraint -> Text
+showTableConstraint = \case
+  PrimaryKey cols -> "PRIMARY KEY (" <> uncommas cols <> ")"
+  Unique cols -> "UNIQUE (" <> uncommas cols <> ")"
