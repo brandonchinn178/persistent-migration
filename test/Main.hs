ee21769fb93c543263d2283f63a08d7faa85c650
Brandon Chinn <brandon@leapyear.io>
2018-06-06 11:45:30 -0700

    Fix stylish-haskell

--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,5 @@
-import Database.Persist.MigrationTest (testMigrations)
 import qualified Database.Persist.Migration.Postgres as Postgres
+import Database.Persist.MigrationTest (testMigrations)
 import Test.Tasty
 
 main :: IO ()
