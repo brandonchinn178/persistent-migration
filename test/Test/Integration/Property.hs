0a7db97126d1b8195b8235024434b79f7b13851c
Brandon Chinn <brandon@leapyear.io>
2018-06-14 10:40:24 -0700

    Make Arbitrary instances in Test.Utils.QuickCheck

--- test/Test/Integration/Property.hs
+++ test/Test/Integration/Property.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.Integration.Property (testProperties) where
@@ -7,33 +8,59 @@ module Test.Integration.Property (testProperties) where
 import Control.Monad ((>=>))
 import Control.Monad.Catch (SomeException(..), try)
 import Control.Monad.IO.Class (liftIO)
+import Data.List (nub)
+import Data.Maybe (mapMaybe)
 import Data.Monoid ((<>))
 import Data.Pool (Pool)
-import Database.Persist.Migration
-    (CreateTable(..), MigrateBackend, Migrateable(..))
+import Database.Persist.Migration.Internal
 import Database.Persist.Migration.Utils.Sql (quote)
 import Database.Persist.Sql (SqlBackend, rawExecute)
 import Test.Integration.Utils.RunSql (runSql)
+import Test.QuickCheck
 import Test.QuickCheck.Monadic (monadicIO, pick, run)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
-import Test.Utils.QuickCheck (genCreateTable)
+import Test.Utils.QuickCheck ()
 
 -- | A test suite for testing migration properties.
 testProperties :: MigrateBackend -> IO (Pool SqlBackend) -> TestTree
 testProperties backend getPool = testGroup "properties"
   [ testProperty "Create arbitrary tables" $ monadicIO $ do
-      tables <- pick genCreateTable
-      let createTable = getMigrationText backend >=> mapM_ rawExecute'
-          dropTable = rawExecute' . ("DROP TABLE " <>) . quote . ctName
+      table <- pick arbitrary
+      fkTables <- pick $ getForeignKeyTables table
+      let createTable = getMigrationText backend >=> mapM_ rawExecutePrint
+          dropTable = rawExecutePrint . ("DROP TABLE " <>) . quote . ctName
       runSql' $ do
-        mapM_ createTable tables
-        mapM_ dropTable $ reverse tables
+        mapM_ createTable fkTables
+        createTable table
+        dropTable table
+        mapM_ dropTable fkTables
   ]
   where
     runSql' f = run $ getPool >>= \pool -> runSql pool f
-    rawExecute' sql = try (rawExecute sql []) >>= \case
+    -- if rawExecute fails, show the sql query run
+    rawExecutePrint sql = try (rawExecute sql []) >>= \case
       Right () -> return ()
       Left (SomeException e) -> do
         liftIO $ print sql
         fail $ show e
+
+-- | Get the CreateTable operations that are necessary for the foreign keys in the
+-- given CreateTable operation.
+getForeignKeyTables :: CreateTable -> Gen [CreateTable]
+getForeignKeyTables ct = do
+  tables <- vectorOf (length neededTables) arbitrary
+  return . map modifyTable . zip neededTables $ tables
+  where
+    neededTables = nub $ concatMap (mapMaybe getReferenceTable . colProps) $ ctSchema ct
+    getReferenceTable = \case
+      References (table, _) -> Just table
+      _ -> Nothing
+    isReference = \case
+      References _ -> True
+      _ -> False
+    noFKs = filter (not . isReference) . colProps
+    modifyTable (name, ct') = ct'
+      { ctName = name
+      , ctSchema = map (\col -> col{colProps = noFKs col}) $ ctSchema ct'
+      }
--- test/Test/Utils/QuickCheck.hs
+++ test/Test/Utils/QuickCheck.hs
@@ -4,15 +4,15 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Test.Utils.QuickCheck
-  ( genCreateTable
-  , genColumn
+  ( Identifier(..)
+  -- * Utilities
+  , DistinctList(..)
+  , mapSome
+  , group
   ) where
 
 import Control.Monad ((>=>))
-import Control.Monad.Extra (concatMapM)
-import Data.Function (on)
-import Data.List (nub, nubBy)
-import Data.Maybe (mapMaybe)
+import Data.List (nub)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as Text
@@ -21,93 +21,81 @@ import Database.Persist.Migration
 import Database.Persist.Sql (SqlType(..))
 import Test.QuickCheck
 
--- | Generate an arbitrary CREATE TABLE query. Also generates tables necessary for foreign keys.
-genCreateTable :: Gen [CreateTable]
-genCreateTable = genCreateTable' True Nothing
-  where
-    genCreateTable' withFKs maybeName = do
-      ctName <- maybe genIdent return maybeName
+instance Arbitrary CreateTable where
+  arbitrary = do
+    Identifier ctName <- arbitrary
 
-      -- get names of tables this table can have foreign keys towards
-      numTables <- choose (0, if withFKs then 2 else 0)
-      tableNames <- vectorOf numTables $ genIdent `suchThat` (/= ctName)
+    -- get names of tables this table can have foreign keys towards
+    DistinctList colNames <- arbitrary
+    -- max out at 100 names
+    let colNames' = take 100 $ map unIdent colNames
 
-      -- generate schema
-      let idCol = Column "id" SqlInt32 [NotNull, AutoIncrement]
-          nubCols = nubBy ((==) `on` colName) . filter ((/= "id") . colName)
-      -- max at 100 columns
-      cols <- fmap (nubCols . take 100) . listOf $ genColumn tableNames
-      let ctSchema = idCol : cols
+    -- generate schema
+    DistinctList tableNames <- arbitrary
+    let tableNames' = filter (/= Identifier ctName) tableNames
+    cols <- vectorOf (length colNames') $ genColumn tableNames'
+    let idCol = Column "id" SqlInt32 [NotNull, AutoIncrement]
+        cols' = map (\(name, col) -> col{colName = name}) $ zip colNames' cols
+        ctSchema = idCol : cols'
 
-      -- generate needed tables for foreign keys
-      let getFK = \case
-            References fk -> Just fk
-            _ -> Nothing
-          neededTables = nub . map fst . concatMap (mapMaybe getFK . colProps) $ ctSchema
-          mkCreateTable name = genCreateTable' False $ Just name
-      otherTables <- concatMapM mkCreateTable neededTables
+    -- all of the columns that will be unique
+    uniqueCols <- sublistOf $ map colName cols'
+    let mkUnique names =
+          -- constraint name can be max 63 characters
+          let constraintName = Text.take 63 $ "unique_" <> Text.intercalate "_" names
+          in Unique constraintName names
+        -- unique constraints should not have more than 32 columns
+        max32 l = if length l > 32
+          then take 32 l : max32 (drop 32 l)
+          else [l]
+    uniqueConstraints <- map mkUnique . concatMap max32 <$> group uniqueCols
 
-      -- all of the columns that will be unique
-      uniqueCols <- sublistOf $ map colName cols
-      let mkUnique names =
-            -- constraint name can be max 63 characters
-            let constraintName = "unique_" <> Text.take 56 (Text.intercalate "_" names)
-            in Unique constraintName names
-          -- unique constraints should not have more than 32 columns
-          max32 l = if length l > 32
-            then take 32 l : max32 (drop 32 l)
-            else [l]
-      uniqueConstraints <- map mkUnique . concatMap max32 <$> group uniqueCols
+    let ctConstraints = PrimaryKey ["id"] : uniqueConstraints
 
-      let ctConstraints = PrimaryKey ["id"] : uniqueConstraints
-
-      return $ otherTables ++ [CreateTable{..}]
+    return CreateTable{..}
 
 -- | Generate an arbitrary Column with a possibly pre-determined name.
 --
 -- Also given the set of table names that can be referenced by foreign keys.
-genColumn :: [Text] -> Gen Column
+genColumn :: [Identifier] -> Gen Column
 genColumn tableNames = do
-  colName <- genIdent `suchThat` (/= "id")
+  colName <- fmap unIdent arbitrary `suchThat` (/= "id")
 
-  -- determine foreign key
-  hasFK <- frequency [(1, pure True), (5, pure False)]
-  references <- if hasFK && not (null tableNames)
-    then do
-      foreignKeyTable <- elements tableNames
-      return [References (foreignKeyTable, "id")]
-    else return []
+  references <- if null tableNames
+    then return []
+    else do
+      Identifier table <- elements tableNames
+      arbitrarySingleton 10 $ References (table, "id")
 
   colType <- if null references
     then arbitrary
     else return SqlInt32
 
-  notNull <- sublistOf [NotNull]
-  let colProps = notNull ++ references
+  autoIncrement <- arbitrarySingleton 1 AutoIncrement
+  notNull <- arbitrarySingleton 50 NotNull
+
+  let colProps = notNull ++ autoIncrement ++ references
 
   return Column{..}
-
-{- Helpers -}
-
--- | Generate a random, valid identifier.
-genIdent :: Gen Text
-genIdent = do
-  first <- elements underletter
-  rest <- listOf $ elements $ underletter ++ ['0'..'9']
-  -- identifiers max 63 characters long
-  return . Text.pack . take 63 $ first : rest
   where
-    underletter = '_':['a'..'z']
+    -- get list with x% chance of having the given element and (100 - x)% chance of
+    -- being an empty list
+    arbitrarySingleton x v = frequency [(x, pure [v]), (100 - x, pure [])]
 
--- | Randomly group the given list.
-group :: [a] -> Gen [[a]]
-group = shuffle >=> partition []
-  where
-    partition res [] = return res
-    partition res l = do
-      i <- choose (1, length l)
-      let (first, rest) = splitAt i l
-      partition (first : res) rest
+instance Arbitrary Column where
+  arbitrary = listOf arbitrary >>= genColumn
+
+newtype Identifier = Identifier { unIdent :: Text }
+  deriving (Show,Eq)
+
+instance Arbitrary Identifier where
+  arbitrary = do
+    first <- elements underletter
+    rest <- listOf $ elements $ underletter ++ ['0'..'9']
+    -- identifiers max 63 characters long
+    return . Identifier . Text.pack . take 63 $ first : rest
+    where
+      underletter = '_':['a'..'z']
 
 instance Arbitrary SqlType where
   arbitrary = do
@@ -125,3 +113,37 @@ instance Arbitrary SqlType where
       , SqlDayTime
       , SqlBlob
       ]
+
+{- Utilities -}
+
+newtype DistinctList a = DistinctList { unDistinctList :: [a] }
+  deriving (Show)
+
+instance (Arbitrary a, Eq a) => Arbitrary (DistinctList a) where
+  arbitrary = DistinctList . nub <$> listOf arbitrary
+
+instance Arbitrary Text where
+  arbitrary = Text.pack . getUnicodeString <$> arbitrary
+
+-- | Randomly modify at least one element in the list with the given function.
+mapSome :: (a -> a) -> [a] -> Gen [a]
+mapSome _ [] = return []
+mapSome f l = do
+  i <- choose (0, length l - 1)
+  let (half1, half2) = splitAt i l
+      modify = mapM $ \x -> do
+        shouldModify <- arbitrary
+        return $ if shouldModify then f x else x
+  half1' <- modify half1
+  half2' <- modify $ tail half2
+  return $ half1' ++ [f $ head half2] ++ half2'
+
+-- | Randomly group the given list.
+group :: [a] -> Gen [[a]]
+group = shuffle >=> partition []
+  where
+    partition res [] = return res
+    partition res l = do
+      i <- choose (1, length l)
+      let (first, rest) = splitAt i l
+      partition (first : res) rest
