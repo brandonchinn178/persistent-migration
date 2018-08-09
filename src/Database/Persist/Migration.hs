54b1efeb2a4dfb4884a6a292dec87a6daf8992ac
Brandon Chinn <brandon@leapyear.io>
2018-08-08 17:43:08 -0700

    Re-export everything necessary for RawOperation (#66)
    * Re-export everything necessary for RawOperation
    
    * stylish

--- src/Database/Persist/Migration.hs
+++ src/Database/Persist/Migration.hs
@@ -18,17 +18,18 @@ module Database.Persist.Migration
   , module Database.Persist.Migration.Utils.Sql
   , PersistValue(..)
   , SqlType(..)
+  , rawSql
   ) where
 
 import Control.Monad (unless)
 import Control.Monad.IO.Class (MonadIO)
 import qualified Data.Text as Text
-import Database.Persist (PersistValue(..), SqlType(..))
 import Database.Persist.Migration.Backend
 import Database.Persist.Migration.Core hiding (getMigration, runMigration)
 import Database.Persist.Migration.Operation
 import Database.Persist.Migration.Operation.Types
 import Database.Persist.Migration.Utils.Sql
+import Database.Persist.Sql (PersistValue(..), SqlType(..), rawSql)
 import qualified Database.Persist.Sql as Persistent
 
 -- | True if the persistent library detects more migrations unaccounted for.
